4f66ca1f0a72a3bf4e228e351e71237a0bb95468
JoSunJoo <49556084+JoSunJoo@users.noreply.github.com>
2020-01-06 23:46:33 +0900

    Update PangMain.java

--- 2017-Java-PokemonPang/src/PangMain.java
+++ 2017-Java-PokemonPang/src/PangMain.java
@@ -2,12 +2,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * 한국기술교육대학교 컴퓨터공학부
- * 2017년도 2학기 학기 프로젝트: 포켓몬팡
- * @author 김상진 
- * 포켓몬팡의 메인
- * MVC 모델 이용
+/*
+  포켓몬팡의 메인
+  MVC 모델 이용
  */
 public class PangMain extends Application{
 	private PangGridView view = new PangGridView();
