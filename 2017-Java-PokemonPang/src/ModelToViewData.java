1e17016e6554266518028f95f601373551ec7c33
JoSunJoo <49556084+JoSunJoo@users.noreply.github.com>
2020-01-06 23:44:59 +0900

    Update ModelToViewData.java

--- 2017-Java-PokemonPang/src/ModelToViewData.java
+++ 2017-Java-PokemonPang/src/ModelToViewData.java
@@ -1,11 +1,5 @@
 import java.util.ArrayList;
-
-/**
- * 한국기술교육대학교 컴퓨터공학부
- * 2017년도 2학기 학기 프로젝트: 포켓몬팡
- * @author 김상진 
- * 모델이 뷰에 전달하는 정보
- */
+ // 모델이 뷰에 전달하는 정보
 
 public class ModelToViewData {
 	// size==0이면 전체 갱신
