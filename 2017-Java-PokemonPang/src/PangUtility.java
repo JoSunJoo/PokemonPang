c2e6cde58b667e99ca9d13c7297ec47bb06bfa71
JoSunJoo <49556084+JoSunJoo@users.noreply.github.com>
2020-01-06 23:46:56 +0900

    Update PangUtility.java

--- 2017-Java-PokemonPang/src/PangUtility.java
+++ 2017-Java-PokemonPang/src/PangUtility.java
@@ -7,13 +7,10 @@ import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-/**
- * 한국기술교육대학교 컴퓨터공학부
- * 2017년도 2학기 학기 프로젝트: 포켓몬팡
- * @author 김상진 
- * 포켓몬팡의 유틸리티 클래스: 
- * 1) 뷰, 모델, 컨트럴러에 속하지 않는 static 함수 모음
- * 2) 뷰, 모델, 컨트럴러에 공통으로 필요로 하는 상수 정의
+/*
+  포켓몬팡의 유틸리티 클래스: 
+  1) 뷰, 모델, 컨트럴러에 속하지 않는 static 함수 모음
+  2) 뷰, 모델, 컨트럴러에 공통으로 필요로 하는 상수 정의
  */
 
 public interface PangUtility {
